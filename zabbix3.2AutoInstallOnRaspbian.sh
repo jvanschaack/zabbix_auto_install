a2b2292eedb3c4654af380a8bbd9ddf262b29778
Jonathan <infested@gmail.com>
2017-04-20 12:50:28 -0400

    Update zabbix3.2AutoInstallOnRaspbian.sh
    Changed comments from Japanese to English

--- zabbix3.2AutoInstallOnRaspbian.sh
+++ zabbix3.2AutoInstallOnRaspbian.sh
@@ -1,46 +1,46 @@
 #!/bin/sh
 
-# 本スクリプトはroot権限で実行して下さい。
-# Raspbian(Jessie)でZabbix3.2を自動でインストールします。
-# 必要なパッケージ類等も全て自動でインストールします。
-# DBにはMariaDB、Webにはnginx1.10.3、PHPは7.0を利用しています。
-# パスワードはデフォルトになっている為、必要であれば適宜修正して下さい。
-# php5等古いバージョンのものはアンインストールしますのでご注意ください。
+# Please execute this script as root privilege.
+# Automatically install Zabbix 3.2 with Raspbian (Jessie).
+# All necessary packages etc are also installed automatically.
+# MariaDB is used for DB, nginx1.10.3 for Web, 7.0 for PHP.
+# Password is defaulted, so please modify it if necessary.
+# Please note that older versions such as php 5 will be uninstalled.
 
-# パッケージのアップデートとアップグレードを実施します。
+# Perform package update and upgrade.
 apt-get update -y && apt-get upgrade -y
 
-# 現在のカレントディレクトリを環境変数に格納します。
+# Store the current current directory in the environment variable.
 dir=$(pwd)
 
-# nginx.serviceを/etc/init.d/配下に保存します。
+# Save nginx.service under /etc/init.d/.
 mv nginx /etc/init.d/ && chmod +x /etc/init.d/nginx
 
-# nginxのコンパイルに必要なパッケージをインストールする。
+# Install the package necessary for nginx compilation.
 apt-get install -y gcc checkinstall libpcre3-dev zlib1g-dev libssl-dev geoip-bin libfontconfig1-dev libgd-dev libgeoip-dev libice-dev libjbig-dev libjpeg-dev libjpeg62-turbo-dev liblzma-dev libpthread-stubs0-dev libsm-dev libtiff5-dev libtiffxx5 libvpx-dev libx11-dev libx11-doc libxau-dev libxcb1-dev libxdmcp-dev libxpm-dev libxslt1-dev libxt-dev x11proto-core-dev x11proto-input-dev x11proto-kb-dev xorg-sgml-doctools xtrans-dev expect tcl-expect
 
-# wgetでnginx1.10.3のソースを保存する。
+# Save source of nginx1.10.3 with wget.
 wget https://nginx.org/download/nginx-1.10.3.tar.gz
 
-# 展開しディレクトリ名を変更した後、取得したファイルを削除しnginxディレクトリへ移動します。
+# After expanding and changing the directory name, delete the acquired file and move to the nginx directory.
 tar zxvf nginx-1.10.3.tar.gz && mv nginx-1.10.3 nginx && rm nginx-1.10.3.tar.gz && cd nginx
 
-# コンパイルする。
+# Compile.
 ./configure --group=nginx --user=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid
 make && cd ../
 
 mv nginx_comp.exp nginx/ && cd nginx
 
-# dpkg作成する。手入力を省くためexpectでパッケージを作成する。
+# Create dpkg. Create a package with expect to save manual input.
 expect nginx_comp.exp
 
-# dpkgで/etc/nginxにインストールし、立つ鳥跡を濁さずでゴミを削除する。
+# Install it in / etc / nginx with dpkg, remove the garbage
 dpkg -i nginx_1.10.3-1_armhf.deb && cd && rm -rf ${dir}
 
-# log格納用のディレクトリを作成します。
+# Create a directory to store log.
 mkdir /var/log/nginx
 
-# サービスに登録しておきます。
+# Register the service.
 systemctl enable nginx.service
 
 exit
